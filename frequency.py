da97c88864b8299b43b43c8c70cc5ac28b149c7c
Nintendroid1 <nlpsalazar@gmail.com>
2019-02-08 10:31:00 -0500

    Updated Frequency to include Index of Coincidence Calculation

--- frequency.py
+++ frequency.py
@@ -9,15 +9,31 @@ import sys
 def frequencyAnalyzer(ciphertext: str):
     ciphertext = ciphertext.lower()
     englishAlphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    letterFrequency = [0]*26
+    letterFrequency = [0.0]*26
     for c in ciphertext:
         n = ord(c) - 97 #a=97 in ascii, ord converts to ascii
         letterFrequency[n] += 1
-    for i in range(26):
-        print("%s: %d" % (englishAlphabet[i], letterFrequency[i]))
-        
+    #for i in range(26):
+    #    print("%s: %d" % (englishAlphabet[i], letterFrequency[i]))
+    for i in range(len(letterFrequency)):
+        letterFrequency[i] /=len(ciphertext)
+    return letterFrequency
+
+def indexOfCoincidence(frequency: int):
+    ic = 0
+    for f in frequency:
+        ic += f**2
+    print(ic)
+    
 def main():
-  frequencyAnalyzer('KRLEMPAEPTUAEPKMYTEDDUXUJIZUDKJJICKMEJJWKPYKJJICKMEJDUXUJIZAURPMIRPEKRUDRIPIRJWLEJYUZHIILYYJKZYKRHUEYIRKRCERDKREDXUHPURPAKYPESUYOTKMTOKPTAIHUMEHUMIGJDTEXUVUUREXIKDUDYGMTVJGRDUHYOUHUPTUHUEZJURPWPTUKJJICKMEJDUXUJIZAURPEJYIKRXIJXUDKREDUQGEPUGRDUHYPERDKRCILMIRMUZPYELEKJGHUPIHUMICRKNUEJJPTUZHKRMKZJUYILJICKMKRXIJXUDERDERKREDUQGEPUHKCIHILZHIIL')
+  #englishFrequency = [0.082, 0.015, 0.028, 0.043, 0.127, 0.022, 0.020, 0.061, 0.070, 0.002, 0.008, 0.040, 0.024, 0.067, 0.075, 0.019, 0.001, 0.060, 0.063, 0.091, 0.028, 0.010, 0.023, 0.001, 0.020, 0.001]
+  #indexOfCoincidence(englishFrequency)#0.065601                                                                                                                                                     
   
+  ciphertextA='KRLEMPAEPTUAEPKMYTEDDUXUJIZUDKJJICKMEJJWKPYKJJICKMEJDUXUJIZAURPMIRPEKRUDRIPIRJWLEJYUZHIILYYJKZYKRHUEYIRKRCERDKREDXUHPURPAKYPESUYOTKMTOKPTAIHUMEHUMIGJDTEXUVUUREXIKDUDYGMTVJGRDUHYOUHUPTUHUEZJURPWPTUKJJICKMEJDUXUJIZAURPEJYIKRXIJXUDKREDUQGEPUGRDUHYPERDKRCILMIRMUZPYELEKJGHUPIHUMICRKNUEJJPTUZHKRMKZJUYILJICKMKRXIJXUDERDERKREDUQGEPUHKCIHILZHIIL'
+  indexOfCoincidence(frequencyAnalyzer(ciphertextA))#0.0674346136339764 
+  
+  ciphertextB='UXQIPXYKEPRQMDTKFLMNOMIIXYAMQMXVZOVGMVWGVXESWTBKUMLTQIHOWWCQQXEKBRFKTVRHZYEWAPKPLTFIBBZWSWEVTXFMZBPIFSZSYONRPSYIQZQBEMAXYSDBNOQCHPVGTGTBUQABPKNVQMZCYHTKGMOIQXLDBMPOOAHGTLWCAHQBDERVQDSMEIMZWMAXKDSMVPXYRQPEXNPDRPAZXMAXMVDWVRHYWDRHUXLLRUGKEMHRPOCAGEZNTVTSRMZVPIBDDISEUVFZRXABPKBKZSKMNPXDSMCVUXNQCPQCZNYSSSNQAZAVGMQEZNLVVRMNPYHEFOCQTSDYQXESAP'
+  indexOfCoincidence(frequencyAnalyzer(ciphertextB))#0.04427365988585835                                                                                                                                          
+
 if __name__== "__main__":
   main()
--- vigenere.py
+++ vigenere.py
@@ -1,3 +1,8 @@
+"""
+	MATH 4175 - HW 3
+	Vigenere Cipher encryptor and decryptor
+	Nathaniel Salazar
+"""
 #Decrypts a string that was encrypted by an vigenere cipher and list possible solutions    
 def vigenereDecrypt(ciphertext: str, key: str):
     ciphertext = ciphertext.upper()
@@ -7,14 +12,13 @@ def vigenereDecrypt(ciphertext: str, key: str):
     i=0
     for c in ciphertext:
         n = ord(c) - 65 #A=65 in ascii
-        #print((ord(key[i%(len(key)-1)])-65))
-        n = (n-(ord(key[i%(len(key)-1)])-65))% 26
+        n = (n-(ord(key[i%(len(key))])-65))% 26
         plaintext.append(chr(n+65).lower())
         i += 1
     print("%s" % (''.join(plaintext)));
     plaintext= []
 
-#Decrypts a string that was encrypted by an vigenere cipher and list possible solutions    
+#Encrypts a string that was using an vigenere cipher 
 def vigenereEncrypt(plaintext: str, key: str):
     plaintext = plaintext.lower()
     key = key.lower()
@@ -23,18 +27,23 @@ def vigenereEncrypt(plaintext: str, key: str):
     i=0
     for p in plaintext:
         n = ord(p) - 97 #a=97 in ascii
-        n = (n+(ord(key[i%(len(key)-1)])-97))% 26
-        ciphertext.append(chr(n+97).lower())
+        n = (n+(ord(key[i%(len(key))])-97))% 26
+        ciphertext.append(chr(n+97).upper())
         i += 1
     print("%s" % (''.join(ciphertext)));
     
 def main():
+    ciphertext = "TCXUTSOYMWTDEMXOPKKJOABVXIBABNSCBVISDZPY"
+    plaintext = "themostfertilesourceofinsightishindsight"                                                                                                                     
+    key = "avtif"
+    vigenereDecrypt(ciphertext, key);
+    vigenereEncrypt(plaintext, key)
+    
     ciphertext = "GVFBQJKHRKFNSGZSUSNCTFRXDJGEESTKHVRIFOEJJVPRQHFMRRFDAAEWFMGGJTOHMYIYNMIGRNXXWOEZIDIGSFECRSTKHRKFNBHSYYEQNTIKRDUVRFZDJTUZYYEPZSEOGFWRNGZIDIGSFECRZYKHRLTDEASMVMNMYYIAJXZTBUJIAACFJKFHKYEJHQCBRZQCOJDIZNYZYVR"
-    key = "Hello"
-    key = key.split()
-    for k in key:
-        if(len(k)== 1):continue
-        vigenereDecrypt(ciphertext, k);
-  
+    plaintext = "beforethelawstandsadoorkeepertothisdoorkeepercomesamanfromthecountryandpraysforadmittancetothelawbutthedoorkeepersaysthathecannotgrantadmittanceatthemomenthemanthinksitoverandasksifhewillbeallowedinlater "
+    key = "FRANZ"
+    vigenereDecrypt(ciphertext, key);
+    vigenereEncrypt(plaintext, key)
+
 if __name__== "__main__":
   main()
