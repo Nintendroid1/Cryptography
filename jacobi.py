b5e9c851f525d4237682179256600496d94ca671
Nintendroid1 <nlpsalazar@gmail.com>
2019-12-07 16:18:04 -0500

    script to calculate jacobi symbol

--- jacobi.py
+++ jacobi.py
@@ -1,4 +1,42 @@
 import sys
 
 if __name__=="__main__":
-    
+    if(len(sys.argv) == 3):
+        a = int(sys.argv[1])
+        n = int(sys.argv[2])
+
+        print(str(a) + "/" + str(n))
+        
+        originalA = a
+        originalN = n
+        a = a % n
+        result = 1
+        while(a != 0):
+            while(a % 2 == 0):
+                a /= 2
+                r = n % 8
+                if(n % 8 == 3 or n % 8 == 5):
+                    result *= -1
+                print(str(a) + "/" + str(n))
+            temp = a
+            a = n 
+            n = temp
+            
+            if(a % 4 == 3 and n % 4 == 3):
+                result *= -1
+
+            a = a % n
+            print(str(a) + "/" + str(n))
+
+        if (n == 1):
+            answer = ""
+            if(result == 1):
+                answer = "QR"
+            else:
+                answer = "QNR"
+            print("Result= " + str(result) + " Therefore " +str(originalA) + " is a " + answer + " mod " + str(originalN))
+        else:
+            print("Result= 0 Therefore " + str(originalN) + "|" + str(originalA))
+
+    else:
+        print("Please input a and n to determine if a is a QR or QNR")
--- prime_factorization.py
+++ prime_factorization.py
@@ -1,17 +1,12 @@
 import sys, math
 
 if __name__=="__main__":
-    print(len(sys.argv))
     if(len(sys.argv) == 2):
         m = int(sys.argv[1])
-        print(m)
-
-        #phi_m = phi(m)
-        phi_m = m-1
         
         #Prime factorization
-        temp = phi_m
-        factorBase = [2,3,5,7,11,13,17,19,23,29,31] #Can be expanded
+        temp = m
+        factorBase = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97] #Can be expanded
         factors = [0] * len(factorBase)
 
         for i in range(len(factorBase)):
@@ -22,7 +17,6 @@ if __name__=="__main__":
                     if(temp == 0):break
 
                     factors[i] += 1 #Increment powers
-                    print("Here")
         print(factors)
     else:
         print("Please input m to prime factorize")
