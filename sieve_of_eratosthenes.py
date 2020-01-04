a5a4e995330f02495745540999fff2ca46fea96f
Nintendroid1 <nlpsalazar@gmail.com>
2020-01-04 13:12:26 -0500

    Completed sieve of eratosthenes

--- sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes.py
@@ -1,38 +1,23 @@
-# Sieve of Eratosthenes
-# Code by David Eppstein, UC Irvine, 28 Feb 2002
-# http://code.activestate.com/recipes/117119/
+import sys, math
 
-def gen_primes():
-    """ Generate an infinite sequence of prime numbers.
-    """
-    # Maps composites to primes witnessing their compositeness.
-    # This is memory efficient, as the sieve is not "run forward"
-    # indefinitely, but only as long as required by the current
-    # number being tested.
-    #
-    D = {}
+#https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+def sieve_of_eratosthenes(limit):
+    ceil_sqrt_limit = int(math.ceil(math.sqrt(limit)))
+    vals = [True] * limit
+    for i in range(2, ceil_sqrt_limit):
+        if vals[i]:
+            for j in range(pow(i,2), len(vals), i):
+                vals[j] = False
     
-    # The running integer that's checked for primeness
-    q = 2
-    
-    while True:
-        if q not in D:
-            # q is a new prime.
-            # Yield it and mark its first multiple that isn't
-            # already marked in previous iterations
-            # 
-            yield q
-            print(q)
-            D[q * q] = [q]
-        else:
-            # q is composite. D[q] is the list of primes that
-            # divide it. Since we've reached q, we no longer
-            # need it in the map, but we'll mark the next 
-            # multiples of its witnesses to prepare for larger
-            # numbers
-            # 
-            for p in D[q]:
-                D.setdefault(p + q, []).append(p)
-            del D[q]
-        
-        q += 1
+    primes = []
+    for k in range(len(vals)):
+        if vals[k]:
+            primes.append(k)
+            
+    print(primes[2:])
+
+if __name__=="__main__":
+    if(len(sys.argv) == 2):
+        sieve_of_eratosthenes(int(sys.argv[1]))
+    else:
+        print("Input a limit n to generate primes from 2 to n")
